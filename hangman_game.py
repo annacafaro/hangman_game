f6d4b1470d7ae4db050a9f70f6ffe39e0bb78c56
Anna Cafaro <35750659+annacafaro@users.noreply.github.com>
2022-09-07 11:36:27 -0400

    Adding files

---
+++ hangman_game.py
@@ -0,0 +1,41 @@
+"""THE HANGMAN GAME
+Guess the letters to a secret word before the hangman is drawn.
+
+Author: Anna Cafaro
+Python Version: 3.9.4
+This code is available at 
+
+Features for new realeses: 
+- update_screen() with any OS
+"""
+
+
+
+
+from numpy.random import randint
+from os import system
+import menu_and_ascii_sprites as SPRITES
+
+
+FILE_PATH = './files/data.txt'
+
+
+def update_screen():
+    system('cls')
+
+
+def red_data(file_path):
+    words = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            words.append(line.stip().upper())
+    return words
+
+
+def main():
+    #data = read_data(FILE_PATH)
+    print(SPRITES.menu)
+
+
+if __name__ == '__main__':
+    main()

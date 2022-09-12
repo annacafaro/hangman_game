b0d0ef2e7ccd229d8945baafff77241dfacef3cc
Anna Cafaro <annacafaroc@gmail.com>
2022-09-12 15:53:33 -0400

    get_player_guess function v.1 develop

--- hangman_game.py
+++ hangman_game.py
@@ -6,8 +6,10 @@ Python Version: 3.9.4
 This code is available at 
 
 Features for new realeses: 
+- improve the while-loop of get_player_guess
 - add sprites for winner and loser
 - ask the language of the words and create a file with english words
+- increase the difficulty of the words and add a clue system
 """
 
 
@@ -22,7 +24,7 @@ import menu_and_ascii_sprites as SPRITES
 FILE_PATH = './files/data.txt'
 
 
-def update_screen(active_os):
+def clear_screen(active_os):
     if active_os == 'Windows':
         system('cls')
     elif active_os == 'Linux' or active_os == 'Darwin':
@@ -35,23 +37,44 @@ def read_data(file_path):
     return words
 
 
+def get_player_guess(already_entered):
+    """Returns the letter the player entered. Also, this function 
+    checks whether the input is a valid character and a different
+    letter from the previous inputs.
+    """
+    while True:  # Keep asking until the player enters a valid letter.
+        print('Guess a letter.')
+        guess = input('-> ').lower()
+        if len(guess) != 1:
+            raise ValueError('Please enter a single letter.')
+        elif guess in already_entered:
+            print('You have already guessed that letter. Choose again.')
+        elif not guess.isalpha():
+            raise ValueError('Please enter a letter. Numbers and other characters are not valid')
+        else:
+            return guess
+
+
+
 def main():
 
+    # Detecting the Operative System for the clear_screen function. 
     ACTIVE_OS = platform.system()
-    missed_letters = []
-    right_guessed = []
 
+    # Set up for the variables of the game:
+    missed_letters = []  # List of incorrect letter guesses.
+    right_guessed = []  # List of correct letter guesses.
     data = read_data(FILE_PATH)
-    secret_word = data[randint(0, len(data))]
+    secret_word = data[randint(0, len(data))] # The word the player must guess. 
 
     game_status = int(input(SPRITES.MENU))
 
     if game_status == 1: 
-        pass
+        guess = get_player_guess(missed_letters + right_guessed)
+        print(guess)
     else:
         print(SPRITES.EXIT)
         sys.exit()
-
 
 
 if __name__ == '__main__':
